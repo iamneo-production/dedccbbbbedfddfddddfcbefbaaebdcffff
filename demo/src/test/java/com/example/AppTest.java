d3550903aabb89f80fbc4e70a8f390e557e07bbc
Iamneo <techadmin@iamneo.ai>
2023-08-24 06:30:25 +0000

    Backup Commit

--- demo/src/test/java/com/example/AppTest.java
+++ demo/src/test/java/com/example/AppTest.java
@@ -3,18 +3,59 @@ package com.example;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.testng.annotations.Test;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    ChromeOptions chromeOptions = new ChromeOptions();
+	WebDriver driver = null;
+
+	@BeforeTest
+	public void beforeTest() throws Exception {
+		
+		driver = new RemoteWebDriver(new URL("http://localhost:4444"), chromeOptions);
+	}
+
+	@BeforeTest
+  public void setup() {
+    driver = new ChromeDriver();
+    driver.get("https://flipkart.com");
+  }
+
+  @Test
+  public void findLinksOnPage() {
+    List<WebElement> links = driver.findElements(By.tagName("a"));
+    System.out.println("Number of links on the page: " + links.size());
+    for (WebElement link : links) {
+      System.out.println("Link text: " + link.getText());
+      System.out.println("Link URL: " + link.getAttribute("href"));
+      System.out.println();
     }
+  }
+
+		
+	@AfterTest
+	public void afterTest() {
+		driver.quit();
+	}
+
 }
