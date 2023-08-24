4128fab341c3ac03d46e6ffce405a201f355280a
Iamneo <techadmin@iamneo.ai>
2023-08-24 06:02:06 +0000

    Backup Commit

--- Testing_Selenium_TestNg/demo/src/test/java/com/example/AppTest.java
+++ Testing_Selenium_TestNg/demo/src/test/java/com/example/AppTest.java
@@ -1,61 +1,39 @@
 package com.example;
+// import org.junit.jupiter.api.Test;
+// import org.openqa.selenium.By;
+// import org.openqa.selenium.WebDriver;
+// import org.openqa.selenium.WebElement;
+// import org.openqa.selenium.chrome.ChromeDriver;
 
-//import static org.junit.Assert.assertTrue;
-
-//import org.junit.Test;
-//package ai.iamneo.testing.Testing_Selenium_TestNg;
-
-import org.testng.annotations.Test;
-import java.net.URL;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.BeforeTest;
-import org.testng.Assert;
-import org.testng.annotations.AfterTest;
 import java.util.List;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
 
 public class AppTest {
 
-	ChromeOptions chromeOptions = new ChromeOptions();
-	WebDriver driver = null;
+    
+    public void testCountAndPrintLinks() {
+        // Set the path to your chromedriver executable
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
 
-	@BeforeTest
-	public void beforeTest() throws Exception {
-		
-		driver = new RemoteWebDriver(new URL("http://localhost:4444"), chromeOptions);
-	}
+        // Initialize the WebDriver
+        WebDriver driver = new ChromeDriver();
 
-	@BeforeTest
-  public void setup() {
-    driver = new ChromeDriver();
-    driver.get("https://flipkart.com");
-  }
+        // Navigate to the Flipkart homepage
+        driver.get("https://www.flipkart.com");
 
-  @Test
-  public void findLinksOnPage() {
-    List<WebElement> links = driver.findElements(By.tagName("a"));
-    System.out.println("Number of links on the page: " + links.size());
-    for (WebElement link : links) {
-      System.out.println("Link text: " + link.getText());
-      System.out.println("Link URL: " + link.getAttribute("href"));
-      System.out.println();
+        // Find all links on the page
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+
+        // Print link count
+        System.out.println("Number of links on the page: " + links.size());
+
+        // Print link text and URLs
+        for (WebElement link : links) {
+            String linkText = link.getText();
+            String linkUrl = link.getAttribute("href");
+            System.out.println("Link Text: " + linkText + " - URL: " + linkUrl);
+        }
+
+        // Close the browser
+        driver.quit();
     }
-  }
-
-		
-	@AfterTest
-	public void afterTest() {
-		driver.quit();
-	}
-
 }
-
